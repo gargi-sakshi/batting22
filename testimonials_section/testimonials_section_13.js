c2ec80af8f1ebc742b2c216437bf14346ea564f0
gargi-sakshi <gargisakshi2010@gmail.com>
2022-02-11 00:31:39 +0530

    added files

---
+++ testimonials_section/testimonials_section_13.js
@@ -0,0 +1,21 @@
+// Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel2").owlCarousel({
+    autoplay: false,
+    dots: true,
+    nav: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      900: {
+        items: 1
+      },
+      1100: {
+        items: 1
+      }
+    }
+  });
